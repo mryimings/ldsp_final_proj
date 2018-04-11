89e7fb294878f830197aaf6e42dc0ee4c639e450
stevenbj <jiangbicheng95@gmail.com>
2018-04-11 19:43:40 -0400

    Front-end Server Build

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var path = require('path')
+
+var port = process.env.PORT || 3002
+var app = express()
+
+
+app.get('/', function(req, res) {
+    res.sendFile("index.html", {
+        root: path.join(__dirname, '/views')
+    });
+});
+
+var listenPort = 3002;
+if (process.env.hasOwnProperty('IISNODE_VERSION') && process.env.hasOwnProperty('PORT')) {
+    listenPort = process.env.PORT
+}
+
+app.listen(listenPort, function() {
+    console.log('Tweets Cloud app listening on port ' + listenPort + '!');
+});
+
